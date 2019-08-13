98b922019cd541bb92209c3c31e0052169ca2128
frigir <giorgio.frittoli@gmail.com>
2019-08-13 19:49:11 +0200

    pom profile

--- src/main/java/it/frigir/JavaHelloWorld.java
+++ src/main/java/it/frigir/JavaHelloWorld.java
@@ -5,4 +5,9 @@ public class JavaHelloWorld {
      public String getHello(){
          return "Hello World";
      }
+
+    public static void main(String[] args) {
+        System.out.println(System.getenv("TEST_HOST"));
+    }
+
 }
